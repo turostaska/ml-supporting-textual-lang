cb86aec5248f1d4fc55b06d3419317a6cc97663a
turostaska <rado.albert@gmail.com>
2022-05-18 17:39:22 +0200

    type check

--- src/main/kotlin/symtab/SymtabBuilderVisitor.kt
+++ src/main/kotlin/symtab/SymtabBuilderVisitor.kt
@@ -4,7 +4,10 @@ import kobraBaseVisitor
 import kobraParser.*
 import symtab.extensions.*
 import type.TypeHierarchy
+import type.util.asType
 import type.util.contains
+import type.util.find
+import type.util.findInSubtree
 
 // symtab a hibakeresőben vagy a kódgeneráló visitorban?
 class SymtabBuilderVisitor: kobraBaseVisitor<Any>() {
@@ -42,6 +45,9 @@ class SymtabBuilderVisitor: kobraBaseVisitor<Any>() {
         val name = simpleIdentifier().text
         val type = this.type().text
 
+        if (expression().inferredType.isNotSubtypeOf(type))
+            throw RuntimeException("Invalid value: $type should be given but ${expression().inferredType} found")
+
         if (isNotMember) {
             // We are in constructor scope, so we add the symbol to the symtab
             // todo: assert type of scope
@@ -57,7 +63,15 @@ class SymtabBuilderVisitor: kobraBaseVisitor<Any>() {
         val name = simpleIdentifier().text
         val type = type()?.text ?: expression().inferredType
 
+        if (expression().inferredType.isNotSubtypeOf(type))
+            throw RuntimeException("Invalid value: $type should be given but ${expression().inferredType} found")
+
         currentScope[name] = Symbol(name, type, this.mutability, typeHierarchy)
         return super.visitPropertyDeclaration(this)
     }
+
+    private fun String.subtypeOf(other: String) =
+        typeHierarchy.find(other).findInSubtree(this.asType()) != null
+
+    private fun String.isNotSubtypeOf(other: String) = !this.subtypeOf(other)
 }
