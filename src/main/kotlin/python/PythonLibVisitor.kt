c11a10a34a99cabc7e9ba150b15871d2549796d4
turostaska <rado.albert@gmail.com>
2022-09-22 20:17:31 +0200

    primitive method symbols can be added from std lib header

--- src/main/kotlin/python/PythonLibVisitor.kt
+++ src/main/kotlin/python/PythonLibVisitor.kt
@@ -2,10 +2,14 @@ package python
 
 import com.kobra.Python3Parser
 import com.kobra.Python3ParserBaseVisitor
+import symtab.ClassMethodSymbol
+import symtab.MethodSymbol
 import symtab.Scope
 import symtab.TypeSymbol
-import type.TypeNames
+import type.TypeNames.typeNamesToPythonMap
+import util.filterNonNull
 
+// todo: fáj a szemem, ezt nagyon ki kell takarítani
 class PythonLibVisitor(
     private val globalScope: Scope,
 ) : Python3ParserBaseVisitor<Unit>() {
@@ -17,7 +21,7 @@ class PythonLibVisitor(
         val classNamePy = ctx.NAME().text
         if (classNamePy !in classesToVisit) return
 
-        val className = TypeNames.typeNamesToPythonMap[classNamePy]
+        val className = typeNamesToPythonMap[classNamePy]
             ?: throw RuntimeException("Can't resolve Python type '$classNamePy' to Kobra type")
         currentTypeSymbol = globalScope.resolveType(className)
             ?: throw RuntimeException("Can't find type symbol for type '$className' in global scope")
@@ -31,12 +35,33 @@ class PythonLibVisitor(
 
     override fun visitFuncdef(ctx: Python3Parser.FuncdefContext) {
         val functionName = ctx.NAME().text
-        val paramTypes = ctx.parameters().typedargslist().tfpdef().mapNotNull { it.test()?.text }
+        val params =
+            try {
+                ctx.parameters()?.typedargslist()?.tfpdef()?.filter { it.NAME()?.text != "self" }
+                    ?.mapNotNull { it.NAME()?.text to it.test()?.text }
+                    ?.toMap()?.mapValues {
+                        it.value?.let { typeNamePy ->
+                            val typeName = typeNamesToPythonMap[typeNamePy]!!
+                            globalScope.resolveType(typeName)
+                                ?: throw RuntimeException("Can't find symbol for type '$typeName' in global scope")
+                        }
+                    }?.filterNonNull() ?: emptyMap()
+            } catch (e: Exception) {
+                return
+            }
+
+        val returnType = ctx.test().or_test(0).and_test(0).not_test(0).comparison().expr(0)
+            .xor_expr(0).and_expr(0).shift_expr(0).arith_expr(0).term(0).factor(0).power().atom_expr()
+            .atom().NAME()?.text?.let {
+                if (it !in classesToVisit) return
+                typeNamesToPythonMap[it]
+            }
+
         println("Function definition of ${currentlyVisitedClass?.plus(".$functionName") ?: functionName}")
 
-//        globalScope += currentTypeSymbol?.let {
-//            ClassMethodSymbol(functionName)
-//        } ?: MethodSymbol()
+        globalScope += currentTypeSymbol?.let {
+            ClassMethodSymbol(functionName, returnType, params, currentTypeSymbol!!)
+        } ?: MethodSymbol(functionName, returnType, params)
 
         super.visitFuncdef(ctx)
     }
