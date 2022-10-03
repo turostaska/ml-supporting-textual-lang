afcd3eb33dda057a2a6630cb3eca69f1dd597231
turostaska <rado.albert@gmail.com>
2022-10-03 09:55:59 +0200

    delete unused statement node classes

--- src/main/kotlin/syntax/node/StatementNode.kt
+++ src/main/kotlin/syntax/node/StatementNode.kt
@@ -1,6 +1,5 @@
 package syntax.node
 
-import symtab.TypeSymbol
 import syntax.SyntaxTreeNode
 
 sealed class StatementNode(parent: SyntaxTreeNode): SyntaxTreeNode(parent)
@@ -10,20 +9,4 @@ class ReturnStatementNode(
     val returnStatement: String,
 ): StatementNode(parent) {
     override fun toCode() = "return $returnStatement"
-}
-
-class AssignmentStatementNode(
-    parent: SyntaxTreeNode,
-    val name: String,
-    val type: TypeSymbol,
-    val value: String,
-): StatementNode(parent) {
-    override fun toCode() = "$name: ${type.referencedType.pythonName} = $value"
-}
-
-class ExpressionStatementNode(
-    parent: SyntaxTreeNode,
-    val statement: String,
-): StatementNode(parent) {
-    override fun toCode() = statement
 }
