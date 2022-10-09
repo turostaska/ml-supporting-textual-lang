b31fcbc9a349b387679ec46df6e505dd140adcf3
turostaska <rado.albert@gmail.com>
2022-10-09 20:29:51 +0200

    handling optional types in standard lib

--- src/main/kotlin/python/pythonExtensions.kt
+++ src/main/kotlin/python/pythonExtensions.kt
@@ -17,24 +17,35 @@ fun TfpdefContext.isNotSelf() = this.NAME()?.text != "self"
 val TestContext.atomExpr get() = this.or_test(0)?.and_test(0)?.not_test(0)?.comparison()?.expr(0)
     ?.xor_expr(0)?.and_expr(0)?.shift_expr(0)?.arith_expr(0)?.term(0)?.factor(0)?.power()?.atom_expr()
 
+fun Atom_exprContext.isOptionalType() = this.atom()?.text == "Optional"
+
 fun TestContext.isUnionType() = this.atomExpr?.atom()?.text == "Union"
 
-fun TestContext.compatibleTypes(): List<String> = if (this.isUnionType()) {
-    this.atomExpr?.trailer(0)?.subscriptlist()?.subscript_()?.mapNotNull { it.text } ?: emptyList()
+fun TestContext.isOptionalType() = this.atomExpr?.isOptionalType() ?: false
+
+fun TestContext.compatibleTypes() = if (this.isUnionType()) {
+    this.atomExpr?.trailer(0)?.subscriptlist()?.subscript_()?.mapNotNull { it?.test(0)?.atomExpr } ?: emptyList()
 } else {
-    listOf(this.text)
+    listOf(this.atomExpr)
 }
 
-val FuncdefContext.parameterNamesToTypeNameMap
+fun Atom_exprContext.toKobraTypeName() = if (this.isOptionalType()) {
+    val typeNamePy = trailer(0).subscriptlist().subscript_(0).test(0).text
+    TypeNames.pythonTypeNamesToKobraMap[typeNamePy]?.plus("?")
+        ?: throw RuntimeException("No mapping for Python type '${this.text}'")
+} else {
+    val typeNamePy = atom().NAME().text
+    TypeNames.pythonTypeNamesToKobraMap[typeNamePy]
+        ?: throw RuntimeException("No mapping for Python type '${this.text}'")
+}
+
+val FuncdefContext.parameterNamesToTypeNameMap: Map<String, List<String>>
     get() = this.parameters()?.typedargslist()?.tfpdef()?.filter { it.isNotSelf() }
         ?.mapNotNull { it.NAME()?.text to it.test().compatibleTypes() }
         ?.toMap()
         // If value is union: list of all types; else: list of the one listed type
         ?.mapValues {
             it.value.let { typeNamesPy ->
-                typeNamesPy.map { typeNamePy ->
-                    TypeNames.pythonTypeNamesToKobraMap[typeNamePy]
-                        ?: throw RuntimeException("No mapping for Python type '$typeNamePy'")
-                }
+                typeNamesPy.mapNotNull { typeNamePy -> typeNamePy?.toKobraTypeName() }
             }
         }?.filterNonNull() ?: emptyMap()
